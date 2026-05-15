ac99883726907571d59cb13358f69341b0208913
Lika <borisovalika2@gmail.com>
2026-05-15 15:13:25 +0300

    task_5: updated apiPaths with importFile endpoint

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -5,22 +5,19 @@ export const ProductSchema = Yup.object({
   title: Yup.string().required().default(""),
   description: Yup.string().default(""),
   price: Yup.number().positive().required().defined().default(0),
-  image: Yup.string().url().nullable().notRequired()
+  image: Yup.string().url().nullable().notRequired(),
 });
 
 export const AvailableProductSchema = ProductSchema.shape({
   count: Yup.number().integer().min(0).required().defined().default(0),
 });
 
-export type Product = Omit<
-  Yup.InferType<typeof ProductSchema>,
-  'image'
-> & {
+export type Product = Omit<Yup.InferType<typeof ProductSchema>, "image"> & {
   image?: string | null;
 };
 export type AvailableProduct = Omit<
   Yup.InferType<typeof AvailableProductSchema>,
-  'image'
+  "image"
 > & {
   image?: string | null;
 };
